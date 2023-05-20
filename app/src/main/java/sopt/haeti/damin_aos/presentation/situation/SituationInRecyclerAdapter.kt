18166af801434f3db27b6dc9686c8346ea15c3e4
sub101 <subin10@nate.com>
2023-05-21 03:16:50 +0900

    situation adapter 구축

--- app/src/main/java/sopt/haeti/damin_aos/presentation/situation/SituationInRecyclerAdapter.kt
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/situation/SituationInRecyclerAdapter.kt
@@ -1,4 +1,33 @@
 package sopt.haeti.damin_aos.presentation.situation
 
-class SituationInRecyclerAdapter {
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import sopt.haeti.damin_aos.databinding.ItemSituationInBinding
+
+class SituationInRecyclerAdapter (context: Context, private val itemClick: (String) -> Unit) :
+    RecyclerView.Adapter<SituationInViewHolder>() {
+    private var modelList: List<SituationInModel> = emptyList()
+    private val layoutInflater by lazy { LayoutInflater.from(context) }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SituationInViewHolder {
+        val binding = ItemSituationInBinding.inflate(layoutInflater, parent, false)
+        return SituationInViewHolder(binding, itemClick)
+    }
+
+    override fun getItemCount(): Int = modelList.size
+
+    override fun onBindViewHolder(holder: SituationInViewHolder, position: Int) {
+        holder.bind(this.modelList[position])
+        Log.d("onBind 확인", "submitList : $this.modelList")
+    }
+
+    fun submitList(modelList: List<SituationInModel>) {
+        this.modelList = modelList
+        notifyDataSetChanged()
+        Log.d("submitList 확인", "submitList : $modelList")
+    }
+
 }
