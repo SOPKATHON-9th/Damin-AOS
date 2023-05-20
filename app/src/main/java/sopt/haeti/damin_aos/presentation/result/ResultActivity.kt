2a037b9b042ca4856916d8d2e33d608961140ee2
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-21 06:21:44 +0900

    feat: 결과 화면에서 버튼 네비게이션 구현

--- app/src/main/java/sopt/haeti/damin_aos/presentation/result/ResultActivity.kt
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/result/ResultActivity.kt
@@ -1,5 +1,6 @@
 package sopt.haeti.damin_aos.presentation.result
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -7,6 +8,8 @@ import androidx.fragment.app.replace
 import com.google.android.material.tabs.TabLayout
 import sopt.haeti.damin_aos.R
 import sopt.haeti.damin_aos.databinding.ActivityResultBinding
+import sopt.haeti.damin_aos.presentation.home.HomeActivity
+import sopt.haeti.damin_aos.presentation.mypage.MypageActivity
 import sopt.haeti.damin_aos.util.base.BindingActivity
 
 class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_result) {
@@ -14,6 +17,21 @@ class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_
         super.onCreate(savedInstanceState)
 
         initTabSelectedListener()
+        initNavigationButtons()
+    }
+
+    private fun initNavigationButtons() {
+        binding.btnMypage.setOnClickListener {
+            Intent(this, MypageActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+
+        binding.btnHome.setOnClickListener {
+            Intent(this, HomeActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 
     private fun initTabSelectedListener() {
