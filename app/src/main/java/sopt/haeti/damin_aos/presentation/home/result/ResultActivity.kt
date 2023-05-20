ec085a003e0aed76f8b32d61516bfbb4cbe79226
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-21 01:19:38 +0900

    결과 화면 레이아웃 구성

---
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/home/result/ResultActivity.kt
@@ -0,0 +1,14 @@
+package sopt.haeti.damin_aos.presentation.home.result
+
+import android.os.Bundle
+import sopt.haeti.damin_aos.R
+import sopt.haeti.damin_aos.databinding.ActivityResultBinding
+import sopt.haeti.damin_aos.util.base.BindingActivity
+
+class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_result) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/home/result/ResultFragment.kt
@@ -0,0 +1,13 @@
+package sopt.haeti.damin_aos.presentation.home.result
+
+import android.os.Bundle
+import android.view.View
+import sopt.haeti.damin_aos.R
+import sopt.haeti.damin_aos.databinding.FragmentResultBinding
+import sopt.haeti.damin_aos.util.base.BindingFragment
+
+class ResultFragment : BindingFragment<FragmentResultBinding>(R.layout.fragment_result) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
