bbea353a456f18171a9651a69d6016a88cd04d8f
taeheeL <t9h9@soongsil.ac.kr>
2023-05-21 02:02:41 +0900

    splash 최종

--- app/src/main/java/sopt/haeti/damin_aos/presentation/SplashActivity.kt
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/SplashActivity.kt
@@ -1,18 +1,26 @@
 package sopt.haeti.damin_aos.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import sopt.haeti.damin_aos.R
-import sopt.haeti.damin_aos.databinding.ActivitySplashBinding
 import sopt.haeti.damin_aos.presentation.home.MainActivity
-import sopt.haeti.damin_aos.util.base.BindingActivity
 import sopt.haeti.damin_aos.util.extension.startActivity
 
-class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private val splashList = listOf(R.layout.activity_splash_fire, R.layout.activity_splash_raining, R.layout.activity_splash_earthquake, R.layout.activity_splash_virus)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val randomSplashLayout = splashList.random()
+        setContentView(randomSplashLayout)
+
         lifecycleScope.launch {
             delay(2000)
             startActivity<MainActivity>()
