c6eafd7d3ab48e65734a8b69bae6e71f34e683d2
sub101 <subin10@nate.com>
2023-05-21 03:04:15 +0900

    situationIn,Where xml & item xml 파일 생성

---
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/situation/SituationInActivity.kt
@@ -0,0 +1,12 @@
+package sopt.haeti.damin_aos.presentation.situation
+
+import android.os.Bundle
+import sopt.haeti.damin_aos.R
+import sopt.haeti.damin_aos.databinding.ActivitySituationInBinding
+import sopt.haeti.damin_aos.util.base.BindingActivity
+
+class SituationInActivity : BindingActivity<ActivitySituationInBinding>(R.layout.activity_situation_in) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/situation/SituationWhereModel.kt
@@ -0,0 +1,5 @@
+package sopt.haeti.damin_aos.presentation.situation
+
+data class SituationWhereModel(
+    val title: String,
+)
