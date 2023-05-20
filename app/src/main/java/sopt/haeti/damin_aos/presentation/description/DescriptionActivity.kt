dbf4b8d05ddfd075de9c7ce53d7f5c2c1ac447b8
sommac <salth6@naver.com>
2023-05-21 03:42:27 +0900

    MyPage Activity & Layout 기초 작업

--- app/src/main/java/sopt/haeti/damin_aos/presentation/description/DescriptionActivity.kt
+++ app/src/main/java/sopt/haeti/damin_aos/presentation/description/DescriptionActivity.kt
@@ -5,21 +5,24 @@ import android.os.Bundle
 import sopt.haeti.damin_aos.R
 import sopt.haeti.damin_aos.databinding.ActivityDescriptionBinding
 import sopt.haeti.damin_aos.presentation.home.MainActivity
+import sopt.haeti.damin_aos.presentation.mypage.MypageActivity
 import sopt.haeti.damin_aos.util.base.BindingActivity
 
-class DescriptionActivity : BindingActivity<ActivityDescriptionBinding>(R.layout.activity_description) {
+class DescriptionActivity :
+    BindingActivity<ActivityDescriptionBinding>(R.layout.activity_description) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
     }
-    private fun initView(){
-        binding.btnHome.setOnClickListener(){
-            intent=Intent(this@DescriptionActivity,MainActivity::class.java)
+
+    private fun initView() {
+        binding.btnHome.setOnClickListener() {
+            intent = Intent(this@DescriptionActivity, MypageActivity::class.java)
             finish()
             startActivity(intent)
         }
-        binding.btnNext.setOnClickListener(){
-            intent = Intent(this@DescriptionActivity, MainActivity::class.java)
+        binding.btnNext.setOnClickListener() {
+            intent = Intent(this@DescriptionActivity, MypageActivity::class.java)
             finish()
             startActivity(intent)
         }
